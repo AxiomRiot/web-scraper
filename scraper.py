42a87747f41d86f59f54f61bf414520e0c987c65
Zachery Knopp <zacheryknopp@gmail.com>
2025-01-20 21:07:55 -0500

    Added feature to handle multiple sections of ingredients and instructions

--- scraper.py
+++ scraper.py
@@ -3,7 +3,12 @@ from scrapers.woks_of_life import scrape_woks_of_life
 def main():
   
   (ingredients, recipe) = scrape_woks_of_life('https://thewoksoflife.com/steamed-shanghai-soup-dumplings-xiaolongbao/')
-  print(ingredients)
+
+  for ingredient in ingredients:
+    print(f"{ingredient[0]} {ingredient[1]} {ingredient[2]} {ingredient[3]}")
+
+  for index, step in enumerate(recipe):
+    print(f"{index} {step}")
 
 if __name__ == "__main__":
   main()
--- scrapers/woks_of_life.py
+++ scrapers/woks_of_life.py
@@ -11,22 +11,24 @@ def scrape_woks_of_life(url):
 
   # Parsing the HTML
   soup = BeautifulSoup(r.content, 'html.parser')
-  ingredient_list = soup.find('ul', class_='wprm-recipe-ingredients')
-  recipe_list = soup.find('ul', class_='wprm-recipe-instructions')
 
   ingredients = []
   recipe = [] 
-  for li in ingredient_list.find_all('li', class_='wprm-recipe-ingredient'):
 
-    ingredients.append((
-      get_ingredient(li, 'wprm-recipe-ingredient-amount'),
-      get_ingredient(li, 'wprm-recipe-ingredient-unit'),
-      get_ingredient(li, 'wprm-recipe-ingredient-name'),
-      get_ingredient(li, 'wprm-recipe-ingredient-notes'),
-    ))
+  for ingredient_group in soup.find_all('div', class_='wprm-recipe-ingredient-group'):
+    ingredient_list = ingredient_group.find('ul', class_='wprm-recipe-ingredients')
+    for li in ingredient_list.find_all('li', class_='wprm-recipe-ingredient'):
+      ingredients.append((
+        get_ingredient(li, 'wprm-recipe-ingredient-amount'),
+        get_ingredient(li, 'wprm-recipe-ingredient-unit'),
+        get_ingredient(li, 'wprm-recipe-ingredient-name'),
+        get_ingredient(li, 'wprm-recipe-ingredient-notes'),
+      ))
 
-  for li in recipe_list.find_all('li', class_='wprm-recipe-instruction'):
-    recipe_div = li.find('div', class_='wprm-recipe-instruction-text')
-    recipe.append(recipe_div.find('span').text)
+  for recipe_group in soup.find_all('div', class_='wprm-recipe-instruction-group'):
+    recipe_list = recipe_group.find('ul', class_='wprm-recipe-instructions')
+    for li in recipe_list.find_all('li', class_='wprm-recipe-instruction'):
+      recipe_div = li.find('div', class_='wprm-recipe-instruction-text')
+      recipe.append(recipe_div.text) if recipe_div.text else recipe.append(recipe_div.find('span').text)
 
   return (ingredients, recipe)
